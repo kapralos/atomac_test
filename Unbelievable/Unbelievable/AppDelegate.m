1726acddabfeda123a1c1a628ae85e04848394a3
Evgeniy Kapralov <evkapral@gmail.com>
2014-05-12 14:51:16 +0400

    Link UI elements to source vars

--- Unbelievable/Unbelievable/AppDelegate.m
+++ Unbelievable/Unbelievable/AppDelegate.m
@@ -8,11 +8,32 @@
 
 #import "AppDelegate.h"
 
+@interface AppDelegate()
+
+@property (weak) IBOutlet NSTextField* inputTextField;
+@property (weak) IBOutlet NSButton* prefixCheckBox;
+@property (weak) IBOutlet NSButtonCell* upperCaseRadio;
+@property (weak) IBOutlet NSButtonCell* lowerCaseRadio;
+@property IBOutlet NSTextView* resultTextView;
+
+- (IBAction)selectTransformButton:(id)sender;
+
+@end
+
 @implementation AppDelegate
 
 - (void)applicationDidFinishLaunching:(NSNotification *)aNotification
 {
     // Insert code here to initialize your application
+    [self.inputTextField setStringValue:@"ololo"];
+    [self.prefixCheckBox setState:NSOffState];
+    self.upperCaseRadio.state = NSOnState;
+    self.lowerCaseRadio.state = NSOnState;
+}
+
+- (void)selectTransformButton:(id)sender
+{
+    [self.resultTextView setString:@"ololo"];
 }
 
 @end
